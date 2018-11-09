5730b080743b5ad6a16130b97dd7edafb893ad8d
corentingiraud <corentin.giraud@insa-lyon.fr>
2018-11-09 13:44:49 -0500

    Finish crypto TP2 ex2

--- Automn session/Cryptography/TP2/Exercice 2/README.md
+++ Automn session/Cryptography/TP2/Exercice 2/README.md
@@ -1,9 +1,16 @@
 # Passwords global wallet C++ Crypto
 
-A very simple c++ program to save passwords in a file using AES 128 CTR mode.
+A very simple c++ program to save passwords in a file in a secure way using AES 128 CTR mode.
+
+For ubuntu, you have to install the following packages:
+`sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils --fix-missing`
 
 To compile:
 `g++ ex2.cpp -o ex2.out -lcryptopp`
 
-Example:
-- `ex2 -a 123pwd -srv facebook -url www.facebook.com -user alice@gmail.com -pwd 12t3r`
+## Examples
+
+- `./ex2 -a 123pwd -srv facebook -url www.facebook.com -user alice@gmail.com -pwd 12t3r`: secure the wallet with 123pwd secret. Add facebook service (url: www.facebook.com) with username: alice@gmail.com and pwd: 12t3r
+- `./ex2.out -l 123pwd`: list all services in the secure wallet
+- `./ex2.out -d 123pwd -i 1 -user`: display a specific service indentified by its line index (-i option) and display only username
+- `./ex2.out -d 123pwd -i 1 -user -pwd`: display a specific service indentified by its line index (-i option) and display username and password
--- Automn session/Cryptography/TP2/Exercice 2/ex2.cpp
+++ Automn session/Cryptography/TP2/Exercice 2/ex2.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include "cryptopp/modes.h"
 #include "cryptopp/aes.h"
@@ -34,21 +35,151 @@ void displayBytes(byte *b)
     cout << output << endl;
 }
 
+string hexEncode(string text)
+{
+    string encoded;
+    StringSource ss2(text, true,
+                     new HexEncoder(
+                         new StringSink(encoded)));
+    return encoded;
+}
+
+string hexDecode(string text)
+{
+    string decoded;
+    StringSource ss2(text, true,
+                     new HexDecoder(
+                         new StringSink(decoded)));
+    return decoded;
+}
+
+string cryptAndEncode(string plainText)
+{
+    byte ctr[AES::BLOCKSIZE];
+    AutoSeededRandomPool prng;
+    prng.GenerateBlock(ctr, sizeof(ctr));
+    string cipher, ctrString;
+    StringSource s1(ctr, 16, true, new StringSink(ctrString));
+
+    CTR_Mode<AES>::Encryption e;
+    e.SetKeyWithIV(KEY, sizeof(KEY), ctr);
+
+    StringSource ss1(plainText, true,
+                     new StreamTransformationFilter(e,
+                                                    new StringSink(cipher)));
+
+    return hexEncode(ctrString) + "$" + hexEncode(cipher);
+}
+
+string decodeAndDecrypt(string text)
+{
+    // Split CTR and encodedCipherText
+    string delimiter = "$";
+    size_t pos = text.find(delimiter);
+    string encodedCTR = text.substr(0, pos);
+    text.erase(0, pos + delimiter.length());
+    string encodedCipherText = text;
+
+    // Decode encodedCipherText
+    string cipherText;
+    StringSource s1(encodedCipherText, true,
+                     new HexDecoder(
+                         new StringSink(cipherText)));
+
+    // Decode and create CTR
+    string ctrString;
+    StringSource s2(encodedCTR, true,
+                     new HexDecoder(
+                         new StringSink(ctrString)));
+    byte ctr[AES::BLOCKSIZE];
+    for (int i = 0; i < AES::BLOCKSIZE; i++)
+    {
+        ctr[i] = ctrString[i];
+    }
+
+    // Decrypt
+    CTR_Mode<AES>::Decryption d;
+    d.SetKeyWithIV(KEY, sizeof(KEY), ctr);
+    string decrypted;
+    StringSource s3(cipherText, true,
+                     new StreamTransformationFilter(d,
+                                                    new StringSink(decrypted)));
+
+    return decrypted;
+}
+
 void addService(string service, string url, string name, string pwd)
 {
     fstream file;
     file.open(PASSWORD_WALLET_PATH, ios_base::app);
-    file << service << ":" << url << ":" << name << ":" << pwd << endl;
+    file << cryptAndEncode(service) << ":" << cryptAndEncode(url)
+         << ":" << cryptAndEncode(name) << ":" << cryptAndEncode(pwd) << endl;
     file.close();
 }
 
-string crytp(string plainText)
+void displayLine(string line, int lineIndex, bool displayUser, bool displayPwd)
 {
+    string delimiter = ":";
+    size_t pos = 0;
+    vector<string> tokens;
+    while ((pos = line.find(delimiter)) != std::string::npos)
+    {
+        tokens.push_back(line.substr(0, pos));
+        line.erase(0, pos + delimiter.length());
+    }
+    tokens.push_back(line);
 
+    tokens[0] = decodeAndDecrypt(tokens[0]);
+    tokens[1] = decodeAndDecrypt(tokens[1]);
+    tokens[2] = displayUser ? decodeAndDecrypt(tokens[2]) : "*****";
+    tokens[3] = displayUser ? decodeAndDecrypt(tokens[3]) : "*****";
+
+    cout << setw(10) << left << lineIndex
+         << setw(30) << left << tokens[0]
+         << setw(30) << left << tokens[1]
+         << setw(30) << left << tokens[2]
+         << setw(30) << left << tokens[3]
+         << endl;
 }
 
-string decrypt(string cryptedText)
+void displayHeader()
 {
+    cout << setw(10) << left << "ligne"
+         << setw(30) << left << "service"
+         << setw(30) << left << "url"
+         << setw(30) << left << "user"
+         << setw(30) << left << "pwd"
+         << endl;
+}
+
+void listService()
+{
+    displayHeader();
+
+    ifstream ifs(PASSWORD_WALLET_PATH);
+    std::string line;
+    int lineIndex = 1;
+    while (getline(ifs, line))
+    {
+        displayLine(line, lineIndex, false, false);
+        lineIndex++;
+    }
+}
+
+string getLineByIndex(int index)
+{
+    ifstream ifs(PASSWORD_WALLET_PATH);
+    std::string line;
+    int lineIndex = 1;
+    while (getline(ifs, line))
+    {
+        if (lineIndex == index)
+        {
+            break;
+        }
+        lineIndex++;
+    }
+    return line;
 }
 
 int main(int argc, char *argv[])
@@ -61,7 +192,6 @@ int main(int argc, char *argv[])
 
     // Compute the key with MD5 hash algorithm
     computeKey(globalPassword, KEY);
-    displayBytes(KEY);
 
     // Add new service
     if (arg == "-a")
@@ -70,23 +200,22 @@ int main(int argc, char *argv[])
         string url, username, pwd;
         for (int i = 3; i < argc; i = i + 2)
         {
-            cout << i << " " << argv[i] << " " << argv[i + 1] << endl;
             string arg = argv[i];
             if (arg == "-srv")
             {
-                service = argv[i+1];
+                service = argv[i + 1];
             }
             else if (arg == "-url")
             {
-                url = argv[i+1];
+                url = argv[i + 1];
             }
             else if (arg == "-user")
             {
-                username = argv[i+1];
+                username = argv[i + 1];
             }
             else if (arg == "-pwd")
             {
-                pwd = argv[i+1];
+                pwd = argv[i + 1];
             }
         }
         addService(service, url, username, pwd);
@@ -94,10 +223,33 @@ int main(int argc, char *argv[])
     // List all services
     else if (arg == "-l")
     {
+        listService();
     }
     // Display specific service
     else if (arg == "-d")
     {
+        int index;
+        bool displayUser = false;
+        bool displayPwd = false;
+        for(int i = 3; i < argc; i++)
+        {
+            string arg = argv[i];            
+            if (arg == "-i")
+            {
+                index = atoi(argv[i + 1]);
+                i++;
+            }
+            else if (arg == "-pwd")
+            {
+                displayUser = true;
+            }
+            else if (arg == "-user")
+            {
+                displayPwd = true;
+            }
+        }
+        displayHeader();
+        displayLine(getLineByIndex(index), index, displayUser, displayPwd);
     }
     return 0;
 }
